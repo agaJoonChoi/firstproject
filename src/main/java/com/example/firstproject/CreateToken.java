7e00af3fd738a9a8686ba7aef04c5556c80a6069
againme <againme.choi@gmail.com>
2024-05-29 00:18:14 +0900

    access token 가져오기

--- src/main/java/com/example/firstproject/CreateToken.java
+++ src/main/java/com/example/firstproject/CreateToken.java
@@ -8,8 +8,10 @@ import com.wrapper.spotify.requests.authorization.client_credentials.ClientCrede
 import java.io.IOException;
 
 public class CreateToken {
-    private static final String CLIENT_ID = "${8c95a70668cf40edb931e9ba2e9c50d9}";
-    private static final String CLIENT_SECRET = "${980aec70360644ce84c74ff4fc4b7f51}";
+//    private static final String CLIENT_ID = "${8c95a70668cf40edb931e9ba2e9c50d9}";
+//    private static final String CLIENT_SECRET = "${980aec70360644ce84c74ff4fc4b7f51}";
+    private static final String CLIENT_ID = "8c95a70668cf40edb931e9ba2e9c50d9";
+    private static final String CLIENT_SECRET = "980aec70360644ce84c74ff4fc4b7f51";
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(CLIENT_ID).setClientSecret(CLIENT_SECRET).build();
 
     public static String accessToken() {
--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,15 +1,23 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.CreateToken;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
     public String niceToMeetYou(Model model) {
         model.addAttribute("username", "cjy");
+
+        String accessToken = CreateToken.accessToken();
+
+        log.info(accessToken);
         return "greetings"; // templates/greetings.mustache -> 브라우저로 전송
     }
 
