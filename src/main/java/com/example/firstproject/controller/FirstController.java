29a1578e2bdb3e623e6260d972c605d520607999
agaJoonChoi <agarcircle@g.shingu.ac.kr>
2024-06-05 11:11:15 +0900

    0605

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,8 +1,9 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.CreateToken;
+import com.example.firstproject.MusicSearch;
+
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,19 @@ public class FirstController {
         model.addAttribute("username", "cjy");
 
         String accessToken = CreateToken.accessToken();
-
         log.info(accessToken);
+
+        MusicSearch search = new MusicSearch();
+
+        String response = search.search(accessToken, "해야");
+//        log.info(response);
+
+//        JsonObject jsonElement = (JsonObject)JsonParser.parseString(response);
+
+        //log.info(jsonElement.getClass() + ":" + jsonElement + "");
+
+//        log.info(jsonElement.get("tracks")+"");
+
         return "greetings"; // templates/greetings.mustache -> 브라우저로 전송
     }
 
@@ -25,6 +37,18 @@ public class FirstController {
     public String seeYouNext(Model model) {
         model.addAttribute("nickname", "홍길동");
         return "goodbye";
+    }
+
+    @GetMapping("/search")
+    public String SearchView(Model model) {
+        MusicSearch search1 = new MusicSearch();
+
+        String accessToken1 = CreateToken.accessToken();
+        String response1 = search1.search(accessToken1, "keyword");
+        model.addAttribute("response1", response1);
+        log.info(response1);
+        return "musicResult";
+
     }
 }
 
